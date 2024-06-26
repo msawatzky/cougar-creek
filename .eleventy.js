8cd4043b46f7cd7f6dd24ced199c1217c5dc48b8
Matt Sawatzky <m.sawatty@gmail.com>
2024-06-26 10:00:07 -0600

    add quadrat font

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/js");
+    eleventyConfig.addPassthroughCopy("src/assets");
     return {
         dir: {
             input: "src",
